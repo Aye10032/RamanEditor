5a3468203ca930f699bd564a512f09d5e599f87b
Aye10032 <aye10032@gmail.com>
2024-02-27 16:41:53 +0800

    添加工具栏

--- view/NodeEditorInterface.py
+++ view/NodeEditorInterface.py
@@ -1,4 +1,6 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGraphicsView
+from qfluentwidgets import CommandBar, Action, FluentIcon
 
 from common.NodeGraphicsScene import NodeGraphicsScene
 from common.StyleSheet import StyleSheet
@@ -9,6 +11,10 @@ class NodeEditorInterface(QWidget):
         super().__init__(parent=parent)
 
         self.layout = QVBoxLayout()
+
+        self.commandBar = CommandBar(self)
+        self.run = Action(FluentIcon.PLAY, self.tr('run'), self)
+
         self.view = QGraphicsView(self)
         self.scene = NodeGraphicsScene(self)
 
@@ -18,8 +24,12 @@ class NodeEditorInterface(QWidget):
         self.setLayout(self.layout)
         self.layout.setContentsMargins(0, 0, 0, 0)
 
+        self.commandBar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        self.commandBar.addAction(self.run)
+
         self.view.setScene(self.scene)
 
+        self.layout.addWidget(self.commandBar)
         self.layout.addWidget(self.view)
 
         self.set_qss()
